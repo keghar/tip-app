a721b5a620b08cb5edc6fcb619fd313eaaf89ee0
keghar <108595664+keghar@users.noreply.github.com>
2023-07-19 14:24:16 -0500

    intalled prisma connected to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import {
+  LoginButton,
+  LogoutButton,
+  ProfileButton,
+  RegisterButton,
+} from "@/components/buttons";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Tip App</h1>
+      <div>
+        <LoginButton />
+        <RegisterButton />
+        <LogoutButton />
+        <ProfileButton />
+      </div>
     </main>
   );
 }
