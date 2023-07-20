4f10d64e193a6615a87b99019b71dc37e5965207
keghar <108595664+keghar@users.noreply.github.com>
2023-07-20 10:51:09 -0500

    added arist link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { User } from "./user";
+// import { User } from "./user";
 import { LoginButton, LogoutButton } from "@/components/buttons";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -12,10 +13,12 @@ export default async function Home() {
       <div>Hello World</div>
       <LoginButton />
       <LogoutButton />
+      <Link href="/artist">Artist Page</Link>
       <h2>Server call</h2>
+
       {/* <pre>{JSON.stringify(session)}</pre> */}
       <h2>Client call</h2>
-      <User />
+      {/* <User /> */}
     </main>
   );
 }
